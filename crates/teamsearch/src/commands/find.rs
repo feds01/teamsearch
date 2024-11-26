33f6e1ee134c6ba256af85ab538b085335fe3491
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-11-26 00:25:34 +0000

    Implement integration with `grep` within `find` command

--- crates/teamsearch/src/commands/find.rs
+++ crates/teamsearch/src/commands/find.rs
@@ -3,6 +3,9 @@
 use std::{fs::File, path::PathBuf};
 
 use anyhow::Result;
+use grep_printer::{ColorSpecs, Standard, StandardBuilder};
+use grep_regex::RegexMatcher;
+use grep_searcher::SearcherBuilder;
 use itertools::Itertools;
 use log::info;
 use teamsearch_utils::{fs, timed};
@@ -11,6 +14,7 @@ use teamsearch_workspace::{
     resolver::{find_files_in_paths, ResolvedFile},
     settings::Settings,
 };
+use termcolor::StandardStream;
 
 pub fn find(files: &[PathBuf], mut settings: Settings) -> Result<()> {
     let paths: Vec<PathBuf> = files.iter().map(fs::normalize_path).unique().collect();
@@ -47,6 +51,43 @@ pub fn find(files: &[PathBuf], mut settings: Settings) -> Result<()> {
         // @@Todo: warn the user that there were no files to check.
         return Ok(());
     }
+
+    // @@Todo: integrate a cache system here, we should be able to avoid re-linting
+    // already existent files and just skip them.
+    let matcher = RegexMatcher::new(settings.pattern.as_str())?;
+
+    let mut printer = StandardBuilder::new()
+        .heading(true)
+        .stats(true)
+        .path(true)
+        .color_specs(ColorSpecs::default_with_color())
+        .build(termcolor::StandardStream::stdout(termcolor::ColorChoice::Always));
+
+    for entry in files {
+        match entry? {
+            ResolvedFile::Nested(file) | ResolvedFile::Root(file) => {
+                let _ = find_matches(&matcher, &file, &mut printer);
+            }
+        }
+    }
+
     Ok(())
 }
+
+/// Use the `grep` crate to find matches in a file.
+///
+/// We aggregate the matches into a vector of tuples, where the first element
+/// is the line number and the second element is the line itself.
+fn find_matches(
+    matcher: &RegexMatcher,
+    path: &PathBuf,
+    printer: &mut Standard<StandardStream>,
+) -> Result<()> {
+    let mut searcher =
+        SearcherBuilder::new().multi_line(true).before_context(1).after_context(1).build();
+
+    let file = File::open(path)?;
+    searcher.search_file(matcher, &file, printer.sink_with_path(&matcher, path))?;
+
+    Ok(())
 }
